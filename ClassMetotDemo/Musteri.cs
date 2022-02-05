9c149372f916fb782cd60fcf22f6d8a299d5cc69
canberk <canberk@CAN>
2022-02-05 17:01:12 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class Musteri
+    {
+        public String Id { get; set; }
+
+        public String Ad { get; set; }
+
+        public String Soyad { get; set; }
+
+        public Double TCKimlikno { get; set; }
+
+    }
+}
